a4c7e799a0f3061d73473c91c433660b167a82c4
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-09 04:06:19 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { addRestaurant } from './actions/restaurants';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
 export class App extends Component {
-  
-  handleClick = ev => {
-    ev.preventDefault()
-    this.props.addRestaurant()
-  }
+ 
   render() {
     return (
       <div className="App">
         <RestaurantInput />
-<<<<<<< HEAD
-        <button onClick={this.handleClick}>Add Restaurant</button>
-=======
-        <button onClick={handleClick}>Add Restaurant</button>
->>>>>>> 47efe72b696ae163e2d902e755c3980de78910d4
         <Restaurants />
-        <p>{this.props.restaurants.length}</p>
       </div>
     );
   }
 };
-const mapStateToProps = state => {
-  return {restaurant: state.restaurant}
-}
 
-const mapDispatchToProps = dispatch => {
-  return {addRestaurant: () => {dispatch(addRestaurant())}}
-}
-
-<<<<<<< HEAD
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-=======
-const mapDispatchToProps = dispatch => {
-  return {addRestaurant: () => {dispatch(addRestaurant())}}
-}
-
-export connect(mapDispatchToProps)(App);
->>>>>>> 47efe72b696ae163e2d902e755c3980de78910d4
+export default App;
