953b39bf99902d1d9070910392f093d921311d76
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-09 04:04:58 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ export class App extends Component {
     return (
       <div className="App">
         <RestaurantInput />
+<<<<<<< HEAD
         <button onClick={this.handleClick}>Add Restaurant</button>
+=======
+        <button onClick={handleClick}>Add Restaurant</button>
+>>>>>>> 47efe72b696ae163e2d902e755c3980de78910d4
         <Restaurants />
         <p>{this.props.restaurants.length}</p>
       </div>
@@ -29,4 +33,12 @@ const mapDispatchToProps = dispatch => {
   return {addRestaurant: () => {dispatch(addRestaurant())}}
 }
 
+<<<<<<< HEAD
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+=======
+const mapDispatchToProps = dispatch => {
+  return {addRestaurant: () => {dispatch(addRestaurant())}}
+}
+
+export connect(mapDispatchToProps)(App);
+>>>>>>> 47efe72b696ae163e2d902e755c3980de78910d4
